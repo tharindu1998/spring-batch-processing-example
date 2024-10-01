ef2db3176764831491fef8fa6ba284de5988bf11
tharindu1998 <tharindudulshanfdo@gmail.com>
2024-10-01 16:52:37 +0530

    initial

---
+++ src/main/java/com/spring_batch_processing/Person.java
@@ -0,0 +1,5 @@
+package com.spring_batch_processing;
+
+public record Person(String firstName, String lastName) {
+
+}
---
+++ src/test/java/com/spring_batch_processing/SpringBatchProcessingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring_batch_processing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchProcessingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
